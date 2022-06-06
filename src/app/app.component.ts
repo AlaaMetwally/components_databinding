ffd0d89b8fb07d27063df850d14fa698d325b4e4
AlaaMetwally <alaametwally4@gmail.com>
2022-06-06 14:46:15 +0200

    component, databinding, decorators(Input,Output,ViewChild,ContentChild), lifecycle(ngInit, ngOnChanges, ngDoCheck, ngAfterContentInit, ngAfterContentChecked, ngAfterViewInit, ngAfterViewChecked, ngDestroy)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'components_databinding';
+  //title = 'components_databinding';
+
+  serverElements = [{ type: 'server', name: 'Testserver', content: 'Just a test!' }];
+
+  //on html page onServerAdded($event) ==> $event obj { serverName: string, serverContent: string }
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
+
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
+
+  onChangeFirst() {
+    this.serverElements[0].name = 'Changed!';
+  }
+
+  onDestroyFirst() {
+    this.serverElements.splice(0, 1);
+  }
 }
